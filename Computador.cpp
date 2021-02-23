10dd9273fb56c6a714f799e5e66f0722838acaf4
María José Galvis <galvismariaj@gmail.com>
2021-02-23 02:27:20 -0400

    Update Computador.cpp

--- Computador.cpp
+++ Computador.cpp
@@ -1,26 +1,3 @@
-#define FILAS 6
-#define COLUMNAS 7
-#define TAMANIO_MATRIZ sizeof(char)*FILAS*COLUMNAS
-#define JUGADOR_1 'o'
-#define JUGADOR_2 'x'
-#define JUGADOR_CPU_1 JUGADOR_1
-#define JUGADOR_CPU_2 JUGADOR_2
-#define ESPACIO_VACIO ' '
-#define FILA_NO_ENCONTRADA -1
-#define ERROR_COLUMNA_LLENA 2
-#define ERROR_FILA_INVALIDA 4
-#define ERROR_NINGUNO 3
-#define CONECTA 4
-#define CONECTA_ARRIBA 1
-#define CONECTA_DERECHA 2
-#define CONECTA_ABAJO_DERECHA 3
-#define CONECTA_ARRIBA_DERECHA 4
-#define NO_CONECTA 0
-#define COLUMNA_GANADORA_NO_ENCONTRADA -1
-#define MODO_HUMANO_CONTRA_HUMANO 1
-#define MODO_HUMANO_CONTRA_CPU 2
-#define MODO_CPU_CONTRA_CPU 3
-
 void Computador::clonarMatriz(Tablero* original) {
 	
     for (int i = 0 ; i < FILAS ; i++){
@@ -60,7 +37,7 @@ int Computador::getUltimaFila(int columna){
  * */
  
  
-void Computador::getMejorColumna(char equipo){
+void Computador::getMejorColumna(char equipo, int *conteo, int *indice){
 	
 	int contMayor = 0;
 	int iColumnaMayor = -1;
@@ -105,8 +82,8 @@ void Computador::getMejorColumna(char equipo){
 		
 	}
 	
-	this->conteo = contMayor;
-	this->indice = iColumnaMayor;
+	conteo = contMayor;
+	indice = iColumnaMayor;
 	
 }
  
@@ -147,61 +124,58 @@ int Computador::elegirColumna(char equipo, char oponente){
 	
 	int posibleResultado = getColumnaGanadora(equipo);
 	
-	if( copiaTablero->hacerMovimiento(posibleResultado, equipo) ){
+	if ( posibleResultado != -1 ){
 		
 		printf("*elijo ganar*\n");
 		return posibleResultado;
 		
 	}
 	
-	int posibleResultadoOponente = obtenerColumnaGanadora(oponente);
+	int posibleResultadoOponente = getColumnaGanadora(oponente);
 	
-	if ( copiaTablero->hacerMovimiento(posibleResultadoOponente, oponente ))
+	if ( posibleResultadoOponente != -1 ){
+		
+		 printf("*elijo evitar que mi oponente gane*\n");
+		return posibleResultadoOponente;
+		
+	}
 	
 	
+	int conteoCpu, columnaCpu;
 	
-}
-
-int elegirColumnaCpu(char jugador, char tablero[FILAS][COLUMNAS]) {
-    // Voy a comprobar si puedo ganar...
-    int posibleColumnaGanadora = obtenerColumnaGanadora(jugador, tablero);
-    if (posibleColumnaGanadora != COLUMNA_GANADORA_NO_ENCONTRADA) {
-        printf("*elijo ganar*\n");
-        return posibleColumnaGanadora;
-    }
-    // Si no, voy a comprobar si mi oponente gana con el siguiente movimiento, para evitarlo
-    char oponente = obtenerOponente(jugador);
-    int posibleColumnaGanadoraDeOponente = obtenerColumnaGanadora(oponente, tablero);
-    if (posibleColumnaGanadoraDeOponente != COLUMNA_GANADORA_NO_ENCONTRADA) {
-        printf("*elijo evitar que mi oponente gane*\n");
-        return posibleColumnaGanadoraDeOponente;
-    }
-    // En caso de que nadie pueda ganar en el siguiente movimiento, buscaré en dónde se obtiene el mayor
-    // puntaje al colocar la pieza
-    int conteoCpu, columnaCpu;
-    obtenerColumnaEnLaQueSeObtieneMayorPuntaje(jugador, tablero, &conteoCpu, &columnaCpu);
-    int conteoOponente, columnaOponente;
-    obtenerColumnaEnLaQueSeObtieneMayorPuntaje(oponente, tablero, &conteoOponente, &columnaOponente);
-    if (conteoOponente > conteoCpu) {
-        printf("*elijo quitarle el puntaje a mi oponente*\n");
+	getMejorColumna(equipo, &conteoCpu, &columnaCpu);
+	
+	int conteoOponente, columnaOponente;
+	
+	getMejorColumna(ponente, &conteoOponente, &columnaOponente);
+	
+	if (conteoOponente > conteoCpu){
+		
+		printf("*elijo quitarle el puntaje a mi oponente*\n");
         return columnaOponente;
-    } else if (conteoCpu > 1) {
+        
+    }
+	
+	else if (conteoCpu > 1) {
         printf("*elijo colocarla en donde obtengo un mayor puntaje*\n");
         return columnaCpu;
     }
-    // Si no, regresar la central por si está desocupada
-
-    int columnaCentral = obtenerColumnaCentral(jugador, tablero);
-    if (columnaCentral != COLUMNA_GANADORA_NO_ENCONTRADA) {
-        printf("*elijo ponerla en el centro*\n");
+    
+    int columnaCentral = getColumnaCentral(equipo);
+    
+    if (columnaCentral != -1){
+    	printf("*elijo ponerla en el centro*\n");
         return columnaCentral;
-    }
-    // Finalmente, devolver la primera disponible de manera aleatoria
-    int columna = obtenerColumnaAleatoria(jugador, tablero);
-    if (columna != FILA_NO_ENCONTRADA) {
-        printf("*elijo la primera vacía aleatoria*\n");
+	}
+	
+	int columna = getColumnaRandom(equipo);
+	
+	if (columna != -1){
+		printf("*elijo la primera vacía aleatoria*\n");
         return columna;
-    }
-    printf("Esto no debería suceder\n");
+	}
+	
+	printf("Esto no debería suceder\n");
     return 0;
+	
 }
